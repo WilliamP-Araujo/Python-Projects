ce61edb4513d080108eaaa161922ed7af76e8263
William P. Araújo <141167553+WilliamP-Araujo@users.noreply.github.com>
2023-12-01 20:22:43 -0300

    Add files via upload

---
+++ Math operation.py
@@ -0,0 +1,40 @@
+
+
+calculo= 2+2*3
+
+print(calculo)
+
+
+'''Multiploção e divisão como na matematica tem essas operações como prioridade, caso seja necessario que X calculo de 
+subtração ou adição seja executado primeiramente, será necessario inserir ele  entre paraenteses'''
+
+#Operadores de comparação
+
+operadores = 10 == 10
+
+print(operadores)
+
+produto= "banana" != "banana"
+
+produto1 = "maça" == "maça"
+
+print(produto)
+
+print(produto1)
+
+#Assignment operators
+
+x = 10
+#x = x + 5
+#x += 5
+#x -= 5
+#x *= 5
+#x /= 5
+x %=6
+
+print(x)
+
+
+
+
+print(x)
