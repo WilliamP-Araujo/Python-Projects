7fb19c6b9a6e5ec583bfb80a857c65faa051e581
William P. Araújo <141167553+WilliamP-Araujo@users.noreply.github.com>
2023-12-08 21:19:36 -0300

    Add files via upload

---
+++ Comparition multiplication Operator.py
@@ -0,0 +1,12 @@
+
+# Multiple comparison Operators (Multiplos Operadores de Comparaçãp)
+
+print("Insira o valor do seu produto")
+valor= float(input())
+
+20 <= valor <40
+
+if valor >= 20 and valor <40:
+    print("Produto inserido")
+else:
+    print("Produto recusado")
