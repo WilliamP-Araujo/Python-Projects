3da9de6d5045d1e8c2bd33bacd7063edff18b921
William P. Araújo <141167553+WilliamP-Araujo@users.noreply.github.com>
2023-12-14 20:27:24 -0300

    Add files via upload

---
+++ For loop - Strings.py
@@ -0,0 +1,15 @@
+# For loop
+
+for letra in ("google"):
+    print(letra)
+
+palavra = "Espetacular"
+for letra in palavra:
+    print(f" {letra}Esta dentro da palavra Espetacular")
+
+# Lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Loop for
+for numero in numeros:
+    print(numero)
